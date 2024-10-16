ed2fbdd04221ef25f3b11e899f0d475786d4fa8a
Filip.Karlsson11 <https://github.com/FilipKarlsson11>
2024-10-16 08:37:06 +0200

    Initial comment

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String [] args){
         System.out.println("Hejsan användare!");
         System.out.println("Ny sak i detta");
+        System.out.println("En till");
     }
 }
